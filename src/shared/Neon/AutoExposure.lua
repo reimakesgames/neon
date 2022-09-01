8836e047d1bc9a6538236fb70de5b8f0c3cc6670
reimakesgames <reimakesgames.dev@gmail.com>
2022-09-01 19:51:50 +0800

    import legacy optimization

--- src/shared/Neon/AutoExposure.lua
+++ src/shared/Neon/AutoExposure.lua
@@ -51,33 +51,55 @@ local function computeOcclusion(origin, dir)
 	return occlusion
 end
 
+local camera = workspace.CurrentCamera
+local oldOrigin = Vector3.new(0, 0, 0)
+local origin = workspace.CurrentCamera.CFrame.Position
+local sunDir = Lighting:GetSunDirection()
+local dayLight = math.clamp(sunDir.Y * 10, -0.5, 1)
+local indoorLevel = computeOcclusion(oldOrigin, UP * 5000)
+local sunOcclusion = computeOcclusion(oldOrigin, sunDir * 5000)
+local indoorLight = getBrightness(Lighting.Ambient)
+local outdoorLight = getBrightness(Lighting.OutdoorAmbient) * 2
+local SunBlindness = math.clamp(sunDir:Dot(camera.CFrame.LookVector), 0, 1)
+
+local CanCompute = true
+
 function AutoExposure.update(deltaTime)
-	local camera = workspace.CurrentCamera
+	camera = workspace.CurrentCamera
 
 	if not camera then
 		return
 	end
 
-	local origin = camera.CFrame.Position
-	local sunDir = Lighting:GetSunDirection()
-	local dayLight = math.clamp(sunDir.Y * 10, -0.5, 1)
+	origin = camera.CFrame.Position
+	if (oldOrigin - origin).Magnitude < 0.05 then
+		CanCompute = false
+	else
+		CanCompute = true
+	end
+	oldOrigin = origin
+
+	sunDir = Lighting:GetSunDirection()
+	dayLight = math.clamp(sunDir.Y * 10, -0.5, 1)
 
 	if dayLight < 0 then
 		sunDir = Lighting:GetMoonDirection()
 	end
 
-	local indoorLevel = computeOcclusion(origin, UP * 5000)
-	local sunOcclusion = computeOcclusion(origin, sunDir * 5000)
+	if CanCompute then
+		indoorLevel = computeOcclusion(origin, UP * 5000)
+		sunOcclusion = computeOcclusion(origin, sunDir * 5000)
 
-	local SunBlindness = math.clamp(sunDir:Dot(camera.CFrame.LookVector), 0, 1)
+		indoorLight = getBrightness(Lighting.Ambient)
+		outdoorLight = getBrightness(Lighting.OutdoorAmbient) * 2
+	end
+
+	SunBlindness = math.clamp(sunDir:Dot(camera.CFrame.LookVector), 0, 1)
 	SunBlindness = (SunBlindness + 2) * 90
 	SunBlindness = -(math.cos(math.rad(SunBlindness)) + 1)
 
-	local indoorLight = getBrightness(Lighting.Ambient)
-	local outdoorLight = getBrightness(Lighting.OutdoorAmbient)
-
 	local strength = 1 - (outdoorLight + ((indoorLight - outdoorLight) * indoorLevel))
-	local target = sunOcclusion * strength * dayLight * 1.5
+	local target = sunOcclusion * strength * dayLight
 	if sunOcclusion == 0 then
 		target = SunBlindness
 	end
@@ -85,12 +107,13 @@ function AutoExposure.update(deltaTime)
 	local current = Lighting.ExposureCompensation
 
 	if (current < target) then
-		current = math.min(target, current + deltaTime)
+		current = math.min(target, current + deltaTime * 0.5)
 	elseif (current > target) then
-		current = math.max(target, current - deltaTime)
+		current = math.max(target, current - deltaTime * 0.5)
 	end
 
 	Lighting.ExposureCompensation = current
+	Lighting.Bloom.Threshold = 0.75 - (current * 0.5)
 end
 
 return AutoExposure
